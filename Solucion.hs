be047b1bdbe59d841662f4556803376db662ef2e
Gonxa6282 <70608072+Gonxa6282@users.noreply.github.com>
2024-09-23 09:27:02 -0300

    Agrego ej. 2+3, empiezo a realizar ej 4

--- Solucion.hs
+++ Solucion.hs
@@ -19,16 +19,40 @@ vuelosValidos ((c1,d1,t1):(c2,d2,t2):xs) = vueloValido1 && vueloValido2 && (c1 /
         vueloValido1 = vueloValido (c1,d1,t1)
         vueloValido2 = vueloValido (c2,d2,t2)
 
---EJEMPLO VUELOS TODOS DISTINTOS (de 4) = [("Rosario", "Cordoba", 3), ("Cordoba", "Rosario", 3), ("Tucuman", "Rosario", 4.4), ("San Luis", "Rosario", 6)]
---EJEMPLO VUELOS CON REPETIDOS (de 4) =  [("Rosario", "Cordoba", 3), ("Cordoba", "Rosario", 3), ("Rosario", "Cordoba", 3)]
 
+--vueloValido va a verificar que el origen y el destino sean distintos, y que el tiempo de viaje sea mayor a 0
 vueloValido :: Vuelo -> Bool
-vueloValido (c1, c2, t) = c1 /= c2 && t > 0
+vueloValido (c, d, t) = c /= d && t > 0
 
 
 -- EJERCICIO 2
 ciudadesConectadas :: AgenciaDeViajes -> Ciudad -> [Ciudad]
-ciudadesConectadas _ _ = ["BsAs"] -- Borrar y escribir el código correcto
+ciudadesConectadas [] _ = [] -- Caso base 
+ciudadesConectadas [(c1,d1,t1)] ciudad  -- Caso donde en AgenciaDeViajes solo hay 1 elemento
+    | ciudad == c1 = [d1]  -- Si la ciudad es la de origen me da el destino
+    | ciudad == d1 = [c1]  -- Si la ciudad es la de destino me da la de origen
+    | otherwise = [] -- Si la ciudad no está conectada no devuelve ninguna 
+ciudadesConectadas ((c1,d1,t1): vuelos) ciudad -- Caso donde AgenciaDeViajes tiene más de 1 elemento
+    | ciudad == c1 = sacarCiudadesRepetidas([d1] ++ ciudadesConectadas vuelos ciudad)
+    | ciudad == d1 = sacarCiudadesRepetidas([c1] ++ ciudadesConectadas vuelos ciudad)
+    | otherwise = ciudadesConectadas vuelos ciudad 
+
+sacarCiudadesRepetidas :: [Ciudad] -> [Ciudad]
+sacarCiudadesRepetidas [] = []
+sacarCiudadesRepetidas [x] = [x]
+sacarCiudadesRepetidas (x:xs)
+    | not (pertenece1 x xs) = [x] ++ sacarCiudadesRepetidas xs
+    | otherwise = x : (sacarCiudadesRepetidas (sacarCiudadEspecifica x xs)) 
+
+pertenece1 :: Ciudad -> [Ciudad] -> Bool
+pertenece1 _ [] = False
+pertenece1 x (y:ys) = x == y || pertenece1 x ys
+
+sacarCiudadEspecifica :: Ciudad -> [Ciudad] -> [Ciudad]
+sacarCiudadEspecifica _ [] = []
+sacarCiudadEspecifica x (y:ys)
+    | x == y = sacarCiudadEspecifica x ys
+    | otherwise = y : (sacarCiudadEspecifica x ys)
 
 -- EJERCICIO 3
 modernizarFlota :: AgenciaDeViajes -> AgenciaDeViajes
@@ -36,8 +60,20 @@ modernizarFlota _ = [("BsAs","Rosario",9.0)] -- Borrar y escribir el código cor
 
 
 -- EJERCICIO 4
+
 ciudadMasConectada :: AgenciaDeViajes -> Ciudad
-ciudadMasConectada _ = "Rosario" -- Borrar y escribir el código correcto
+ciudadMasConectada [x] = x
+ciudadMasConectada ((c1,d1,t1):(c2,d2,t2):xs) | (numConexiones C1 >= numConexiones C2 && numConexiones C1 >= numConexiones D2) || (numConexionesD1 >= numConexionesD2 && numConexiones D1 >= numConexiones C2) = ciudadMasConectada ((c1,d1,t1):xs)
+                                              | otherwise = ciudadMasConectada ((c2,d2,t2):xs)
+    where
+        numConexionesC1  = longitud (ciudadesConectadas ((c1,d1,t1):(c2,d2,t2):xs) c1)
+        numConexionesC2  = longitud (ciudadesConectadas ((c1,d1,t1):(c2,d2,t2):xs) c2)
+        numConexionesD1 = longitud (ciudadesConectadas ((c1,d1,t1):(c2,d2,t2):xs) d1)
+        numConexionesD2 = longitud (ciudadesConectadas ((c1,d1,t1):(c2,d2,t2):xs) d2)
+
+longitud :: [t] -> Integer
+longitud [] = 0
+longitud (x:xs) = 1 + longitud xs
 
 
 -- EJERCICIO 5
