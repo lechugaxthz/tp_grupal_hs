b1ad31d4bad8fb86870c8827e84a2eb3c6898b96
Estudiante <Estudiante@pc11.lab1111.labos.fcen.uba.ar>
2024-09-24 16:00:36 -0300

    punto 7 terminado en Prueba.hs. mas casos prueba despues!

--- Prueba.hs
+++ Prueba.hs
@@ -38,29 +38,6 @@ diferenteA2Ciudades ((origen, destino, t) : viajes) ciudad1 ciudad2
     difDestinoYC1 = destino /= ciudad1
     difOrigenYC2 = origen /= ciudad2
 
-sacaOrigenYDestino :: [AgenciaDeViajes] -> Ciudad -> Bool
-sacaOrigenYDestino ([] : _ : _ : _) ciudad = False
-sacaOrigenYDestino (_ : _ : [] : _) ciudad = False
-sacaOrigenYDestino (((origen1, destino1, t1) : origenIgual) : diferenteTodo : ((origen3, destino3, t3) : destinoIgual) : conjunto) ciudad
-  | unaEscala = True
-  | masEscalas && esOrigen1Ydestino3 = validaMasEscalas (destino1, diferenteTodo, origen3) || sacaOrigenYDestino (((origen1, destino1, t1) : origenIgual) : diferenteTodo : destinoIgual : conjunto) ciudad || sacaOrigenYDestino (origenIgual : diferenteTodo : ((origen3, destino3, t3) : destinoIgual) : conjunto) ciudad
-  | otherwise = validaMasEscalas (destino3, diferenteTodo, origen1) || sacaOrigenYDestino (((origen1, destino1, t1) : origenIgual) : diferenteTodo : destinoIgual : conjunto) ciudad || sacaOrigenYDestino (origenIgual : diferenteTodo : ((origen3, destino3, t3) : destinoIgual) : conjunto) ciudad
-  where
-    unaEscala = (origen1 == ciudad && destino3 == ciudad && destino1 == origen3)
-    masEscalas = origen1 /= origen3 && (origen1 == ciudad || origen3 == ciudad) && destino1 /= destino3 && (destino1 == ciudad || destino3 == ciudad)
-    esOrigen1Ydestino3 = origen1 == destino3 && origen1 == ciudad
-
-validaMasEscalas :: (Ciudad, AgenciaDeViajes, Ciudad) -> Bool
-validaMasEscalas (_, [], _) = False
-validaMasEscalas (destinoA, (origen, destino, _) : viajes, origenB)
-  | igualAlDestino && igualAlOrigen = True
-  | igualAlDestino = validaMasEscalas (destino, viajes, origenB)
-  | igualAlOrigen = validaMasEscalas (destinoA, viajes, origen)
-  | otherwise = validaMasEscalas (destinoA, viajes, origenB)
-  where
-    igualAlDestino = destinoA == origen
-    igualAlOrigen = origenB == destino
-
 
 validaMasEscalas2 :: [AgenciaDeViajes] -> Bool
 validaMasEscalas2 ([]:_:_: conjunto) = False
